202e92e08dbc52afa8ca32367db53b29d9cc9356
rodrigomilfont <rodrigomilfont@gmail.com>
2017-12-18 10:02:01 -0200

    Hot reload

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -33,6 +33,9 @@ module.exports = {
   // This means they will be the "root" imports that are included in JS bundle.
   // The first two entry points enable "hot" CSS and auto-refreshes for JS.
   entry: [
+    // 'react-hot-loader/patch',
+    // 'webpack-dev-server/client?http://localhost:3000',
+    // 'webpack/hot/only-dev-server',
     // We ship a few polyfills by default:
     require.resolve('./polyfills'),
     // Include an alternative client for WebpackDevServer. A client's job is to
@@ -45,7 +48,7 @@ module.exports = {
     // the line below with these two lines if you prefer the stock client:
     require.resolve('webpack-dev-server/client') + '?/',
     require.resolve('webpack/hot/dev-server'),
-    // require.resolve('react-dev-utils/webpackHotDevClient'),
+    require.resolve('react-dev-utils/webpackHotDevClient'),
     // Finally, this is your app's code:
     paths.appIndexJs,
     // We include the app code last so that if there is a runtime error during
