0869c034181f25e2373a24c6a67d0f201f833619
rodrigomilfont <rodrigomilfont@gmail.com>
2017-12-18 17:25:40 -0200

    flow + -

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,29 @@
 // @flow
 
 import React from 'react';
-import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+const element = document.getElementById('root');
+
 const renderApp = () => {
-	render(
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>,
-		document.getElementById('root')
-	);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    element,
+  );
 };
 
 renderApp();
 
 if (module.hot) {
-	module.hot.accept('./App', () => {
-		renderApp();
-	});
+  module.hot.accept('./App', () => {
+    renderApp();
+  });
 }
 
 registerServiceWorker();
