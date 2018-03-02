c21081634d16904405aba9e794c4eff53e66d230
rodrigomilfont <rodrigomilfont@gmail.com>
2018-03-02 12:10:00 -0300

    trying using setState

--- src/components/home/Home.test.jsx
+++ src/components/home/Home.test.jsx
@@ -1,6 +1,7 @@
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Home from './Home';
 
 import Header from '../header/Header';
@@ -17,8 +18,20 @@ it('example of debug use', () => {
   console.log(wrapper.find('.footer').debug());
 });
 
-it('check if header', () => {
+it('check if header component is instanced', () => {
   const wrapper = shallow(<Home />);
-  // expect(wrapper)
-  console.log(': ', wrapper.find(Header).debug());
+  expect(wrapper.find(Header).length).toEqual(1);
+});
+
+it('set state test', () => {
+  const wrapper = mount(
+    <BrowserRouter>
+      <Home searchTerm="initial value" />
+    </BrowserRouter>,
+  );
+  const header = wrapper.find(Header);
+  wrapper.setProps({ searchTerm: 'value set by test' });
+  // console.log('wrapper: ', wrapper.debug());
+  wrapper.update();
+  // console.log('wrapper: ', wrapper.html());
 });
