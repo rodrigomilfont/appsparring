7afa820ed836f0501ca163aa57efc6ed92cac249
rodrigomilfont <rodrigomilfont@gmail.com>
2018-04-02 16:57:55 -0300

    home libre

---
+++ src/components/searchitens/SearchItens.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SearchItens = props => {
+  console.log('props: ', props);
+  const { searchs } = props;
+  return (
+    <ul>
+      {searchs.map(item => (
+        <li key={item.id}>
+          <p>{item.title}</p>
+          <img src={item.thumbnail} alt={item.title} />
+          <span>
+            {new Date(item.stop_time).toLocaleTimeString('pt-BR', {
+              timeZone: 'UTC',
+            })}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+SearchItens.propTypes = {
+  searchs: PropTypes.arrayOf(PropTypes.object),
+};
+
+SearchItens.defaultProps = {
+  searchs: [],
+};
+
+export default SearchItens;
